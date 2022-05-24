4b74883bd94b1bb8b5a62357bec112128d33ca5e
Alec <amsears60441@gmail.com>
2022-05-24 16:42:48 -0600

    having problems with buttons

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
-function myFunction() {
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function fuck() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
